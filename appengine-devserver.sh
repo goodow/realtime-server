337189dedc335122a61006b9b13b007cc66d9e96
larry <dev@goodow.com>
2013-09-05 16:12:44 +0800

    Set jvm_flag: -Dfile.encoding=UTF-8

--- appengine-devserver.sh
+++ appengine-devserver.sh
@@ -1,5 +1,6 @@
 export DEV_SERVER=$HOME/.m2/repository/com/google/appengine/appengine-java-sdk/1.8.1.1/appengine-java-sdk/appengine-java-sdk-1.8.1.1/bin/dev_appserver.sh
 chmod 770 $DEV_SERVER
 $DEV_SERVER \
+  --jvm_flag=-Dfile.encoding=UTF-8 \
   --disable_update_check --address=0.0.0.0 \
   target/realtime-server-appengine-0.3.0-SNAPSHOT
